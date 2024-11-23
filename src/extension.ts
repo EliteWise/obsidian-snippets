837437ea4db111b0a6c3111bda431d5f5a1f0a84
Florent <eliteman.fm@gmail.com>
2024-11-23 20:16:14 +0100

    Add selected code copy/paste from VSCode to Obsidian feature with current date & extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	let snippetsFolderPath: string = '';
+
 	console.log('Congratulations, your extension "obsidian-snippets" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('obsidian-snippets.setup', () => {
 		vscode.window.showOpenDialog({ canSelectFolders: true, canSelectMany: false, canSelectFiles: false, openLabel: 'Select your Obsidian Vault' }).then(folderUri => {
 			if(folderUri && folderUri[0]) {
 				const obsidianPath = folderUri[0].path;
 				const config = vscode.workspace.getConfiguration();
-				config.update('obsidian-snippets.path', obsidianPath, vscode.ConfigurationTarget.Global).then(() => {
-					console.log('Updated config');
-				});
-				const obsidianPathPersistant = config.get('obsidian-snippets.path');
-				console.log(obsidianPathPersistant);
+				config.update('obsidian-snippets.path', obsidianPath, vscode.ConfigurationTarget.Global);
+				const obsidianPathPersistant = String(config.get('obsidian-snippets.path')).replace('/C:', '');
+				snippetsFolderPath = obsidianPathPersistant + '/Snippets/';
+				if (!fs.existsSync(snippetsFolderPath)) {
+					fs.mkdirSync(snippetsFolderPath);
+					console.log('Snippets folder created!');
+				}
+				
 			}
 		});
 	});
 
+	const copySnippetCommand = () => {
+		const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+		const selection: vscode.Selection | undefined = editor?.selection;
+		const codeSelected: string | undefined = editor?.document.getText(selection);
+
+		const fileExtension = path.extname(editor?.document.fileName).slice(1);
+		const currentDate = new Date().toISOString().split('T')[0];
+
+		const mdFile = snippetsFolderPath + currentDate + '-' + path.basename(`${editor?.document.fileName}.md`);
+		console.log(mdFile);
+		fs.writeFileSync(mdFile, `\`\`\`${fileExtension}\n${codeSelected}\n\`\`\``);
+
+		const openedMdFile = vscode.workspace.openTextDocument(vscode.Uri.file(mdFile));
+		openedMdFile.then(mdFile => {
+			vscode.window.showTextDocument(mdFile);
+		});
+	};
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('obsidian-snippets.copy', copySnippetCommand));
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
